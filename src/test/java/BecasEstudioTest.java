5ab6f994702ee3e7711e1c774d3347577ed93ea6
Luciano Paniagua <erickmallea12345@gmail.com>
2022-02-22 08:30:36 -0400

    tercer commit

--- src/test/java/BecasEstudioTest.java
+++ src/test/java/BecasEstudioTest.java
@@ -1,4 +1,6 @@
+import ejercicioRecuperacion.BecaEstudio;
 import ejercicioRecuperacion.Helpers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -9,11 +11,39 @@ Helpers servicioMock = Mockito.mock(Helpers.class);
 
 @BeforeAll
 public static void before(){
-    MockedStatic<>
+    Helpers servicioMock = Mockito.mock(Helpers.class);
+    MockedStatic<Helpers> mockedStatic =Mockito.mockStatic(Helpers.class);
+    mockedStatic.when(()->Helpers.applicaBeca(12345)).thenReturn(true);
+    mockedStatic.when(()->Helpers.applicaBeca(54321)).thenReturn(true);
+    mockedStatic.when(()->Helpers.applicaBeca(11111)).thenReturn(false);
 }
 
-@Test
-public void verify(){
-    Mockito.when(servicioMock.getNota())
-}
+    @Test
+    public void verifyAplicaBeca(){
+    Mockito.when(servicioMock.getNota(12345)).thenReturn(95);
+    BecaEstudio becaEstudio = new BecaEstudio(servicioMock);
+    String actual =becaEstudio.recomendacionBeca(12345);
+    String expected ="SI APLICA A BECA";
+    Assertions.assertEquals(actual,expected,"EROOR");
+    }
+
+    @Test
+    public void verifyNoAplicaBeca(){
+        Mockito.when(servicioMock.getNota(54321)).thenReturn(50);
+        BecaEstudio becaEstudio = new BecaEstudio(servicioMock);
+        String actual =becaEstudio.recomendacionBeca(54321);
+        String expected ="NO APLICA A BECA POR PROMEDIO ACADEMICO";
+        Assertions.assertEquals(actual,expected,"EROOR");
+    }
+
+    @Test
+    public void verifyAplicaBecaFalse(){
+        Mockito.when(servicioMock.getNota(11111)).thenReturn(100);
+        BecaEstudio becaEstudio = new BecaEstudio(servicioMock);
+        String actual =becaEstudio.recomendacionBeca(12345);
+        String expected ="SI APLICA A BECA";
+        Assertions.assertEquals(actual,expected,"EROOR");
+    }
+
+
 }
